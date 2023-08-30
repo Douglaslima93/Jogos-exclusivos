99a89fa44ccb245f777877cda71c906210061c44
Douglas Lima <douglas07a@gmail.com>
2023-08-30 11:53:13 -0300

    Conteudo Lista de Jogos e lista de jogos

--- src/componentes/ListaJogosExclusivos/index.jsx
+++ src/componentes/ListaJogosExclusivos/index.jsx
@@ -1,1 +1,37 @@
 import './style.css';
+import ItemJogos from "../ItemJogos";
+import { useState } from 'react';
+import { jogosExclusivos } from '../../dados';
+
+const ListaDeJogosExclusivos = () => {
+    
+    const [listaJogos, setListaJogos] = useState(jogosExclusivos);
+
+    const handleFiltrarJogosPlataforma = (plataforma) => {
+        setListaJogos(
+            jogosExclusivos.filter((jogo) => jogo.plataforma === plataforma)
+        )
+    };
+
+    return (
+        <div className='container-principal'>
+            <h2>Lista de Jogos Exclusivos</h2>
+            <div className='container-btns'>
+                <button onClick={() => handleFiltrarJogosPlataforma("xbox")}>XBOX</button>
+                <button onClick={() => handleFiltrarJogosPlataforma("playstation")}>PLAYSTATION</button>
+                <button onClick={() => handleFiltrarJogosPlataforma("nintendo")}>NINTENDO</button>
+            </div>
+            <div className='container-jogos'>
+                {listaJogos.map((jogo) => (
+                    <ItemJogos
+                    key={jogo.id}
+                    nome={jogo.nome}
+                    plataforma={jogo.plataforma}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ListaDeJogosExclusivos;
